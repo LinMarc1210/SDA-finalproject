96c926ddae54fb771e1cf14660e72791115a965a
YJClark <huang.clark0410@gmail.com>
2023-12-12 18:33:34 +0800

    push Level.h

--- Header File/Level.h
+++ Header File/Level.h
@@ -1,11 +1,13 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <vector>
 #include <QTimer>
 #include <QObject>
 #include <QGraphicsScene>
 #include <QGraphicsPixmapItem>
 
+using namespace std;
 
 class Level : public QGraphicsScene
 {
@@ -14,14 +16,23 @@ public:
     Level(int level, int number);
     void setPlayer(int number);
     void generateMap(int level);
-    void generateEnemy();
     bool gamePause();//目前不寫
     void saveGame(); //目前不知怎麼搞
     void showGameover();//最後再寫
-    void showSurvivor();
+    void showSurvivor(int remains);
+
+    void setTankRemainsVector(const vector<vector<char>>& tankRemains);
+    const vector<vector<char>>& getTankRemainsVector() const;
+
+public slots:
+    void generateEnemy(int level);
 
 private:
-    int level;
+//    int level;
+//    int number;
+    int remains =20;
+    vector<vector<char>> tanks;
+
     QGraphicsPixmapItem *survivors;
     QGraphicsPixmapItem *player1;
     QGraphicsPixmapItem *player2;
